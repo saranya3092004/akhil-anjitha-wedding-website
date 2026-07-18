591f47b1fac4ebc35d30e87ada299e19bb96cbab
saranya3092004 <saranyasivakumar3092004@gmail.com>
2026-07-18 13:26:34 +0530

    made changes to images folder structure

--- src/weddingPhotos.ts
+++ src/weddingPhotos.ts
@@ -1,22 +1,22 @@
-import photo01 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.26 PM.jpeg';
-import photo02 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.27 PM (1).jpeg';
-import photo03 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.27 PM.jpeg';
-import photo04 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.28 PM (1).jpeg';
-import photo05 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.28 PM.jpeg';
-import photo06 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.29 PM.jpeg';
-import photo07 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.30 PM (1).jpeg';
-import photo08 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.30 PM.jpeg';
-import photo09 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.31 PM (1).jpeg';
-import photo10 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.31 PM (2).jpeg';
-import photo11 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.31 PM.jpeg';
-import photo12 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.32 PM (1).jpeg';
-import photo13 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.32 PM.jpeg';
-import photo14 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.33 PM (1).jpeg';
-import photo15 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.33 PM.jpeg';
-import photo16 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.34 PM (1).jpeg';
-import photo17 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.34 PM.jpeg';
-import photo18 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.35 PM (1).jpeg';
-import photo19 from '../assets/.aistudio/WhatsApp Image 2026-06-29 at 3.32.35 PM.jpeg';
+import photo01 from './assets/WhatsApp Image 2026-06-29 at 3.32.26 PM.jpeg';
+import photo02 from './assets/WhatsApp Image 2026-06-29 at 3.32.27 PM (1).jpeg';
+import photo03 from './assets/WhatsApp Image 2026-06-29 at 3.32.27 PM.jpeg';
+import photo04 from './assets/WhatsApp Image 2026-06-29 at 3.32.28 PM (1).jpeg';
+import photo05 from './assets/WhatsApp Image 2026-06-29 at 3.32.28 PM.jpeg';
+import photo06 from './assets/WhatsApp Image 2026-06-29 at 3.32.29 PM.jpeg';
+import photo07 from './assets/WhatsApp Image 2026-06-29 at 3.32.30 PM (1).jpeg';
+import photo08 from './assets/WhatsApp Image 2026-06-29 at 3.32.30 PM.jpeg';
+import photo09 from './assets/WhatsApp Image 2026-06-29 at 3.32.31 PM (1).jpeg';
+import photo10 from './assets/WhatsApp Image 2026-06-29 at 3.32.31 PM (2).jpeg';
+import photo11 from './assets/WhatsApp Image 2026-06-29 at 3.32.31 PM.jpeg';
+import photo12 from './assets/WhatsApp Image 2026-06-29 at 3.32.32 PM (1).jpeg';
+import photo13 from './assets/WhatsApp Image 2026-06-29 at 3.32.32 PM.jpeg';
+import photo14 from './assets/WhatsApp Image 2026-06-29 at 3.32.33 PM (1).jpeg';
+import photo15 from './assets/WhatsApp Image 2026-06-29 at 3.32.33 PM.jpeg';
+import photo16 from './assets/WhatsApp Image 2026-06-29 at 3.32.34 PM (1).jpeg';
+import photo17 from './assets/WhatsApp Image 2026-06-29 at 3.32.34 PM.jpeg';
+import photo18 from './assets/WhatsApp Image 2026-06-29 at 3.32.35 PM (1).jpeg';
+import photo19 from './assets/WhatsApp Image 2026-06-29 at 3.32.35 PM.jpeg';
 
 export const weddingPhotos = [
   photo01, photo02, photo03, photo04, photo05, photo06, photo07, photo08, photo09,
