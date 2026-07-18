e4e6df6804f9556ab10f4d9b46f0fae31f9d16a8
saranya3092004 <saranyasivakumar3092004@gmail.com>
2026-07-18 19:59:26 +0530

    removed images

--- src/weddingPhotos.ts
+++ src/weddingPhotos.ts
@@ -3,29 +3,18 @@ import photo02 from './assets/WhatsApp Image 2026-06-29 at 3.32.27 PM (1).jpeg';
 import photo03 from './assets/WhatsApp Image 2026-06-29 at 3.32.27 PM.jpeg';
 import photo04 from './assets/WhatsApp Image 2026-06-29 at 3.32.28 PM (1).jpeg';
 import photo05 from './assets/WhatsApp Image 2026-06-29 at 3.32.28 PM.jpeg';
-import photo06 from './assets/WhatsApp Image 2026-06-29 at 3.32.29 PM.jpeg';
-import photo07 from './assets/WhatsApp Image 2026-06-29 at 3.32.30 PM (1).jpeg';
-import photo08 from './assets/WhatsApp Image 2026-06-29 at 3.32.30 PM.jpeg';
 import photo09 from './assets/WhatsApp Image 2026-06-29 at 3.32.31 PM (1).jpeg';
-import photo10 from './assets/WhatsApp Image 2026-06-29 at 3.32.31 PM (2).jpeg';
-import photo11 from './assets/WhatsApp Image 2026-06-29 at 3.32.31 PM.jpeg';
 import photo12 from './assets/WhatsApp Image 2026-06-29 at 3.32.32 PM (1).jpeg';
-import photo13 from './assets/WhatsApp Image 2026-06-29 at 3.32.32 PM.jpeg';
-import photo14 from './assets/WhatsApp Image 2026-06-29 at 3.32.33 PM (1).jpeg';
 import photo15 from './assets/WhatsApp Image 2026-06-29 at 3.32.33 PM.jpeg';
-import photo16 from './assets/WhatsApp Image 2026-06-29 at 3.32.34 PM (1).jpeg';
-import photo17 from './assets/WhatsApp Image 2026-06-29 at 3.32.34 PM.jpeg';
 import photo18 from './assets/WhatsApp Image 2026-06-29 at 3.32.35 PM (1).jpeg';
-import photo19 from './assets/WhatsApp Image 2026-06-29 at 3.32.35 PM.jpeg';
 
 export const weddingPhotos = [
-  photo01, photo02, photo03, photo04, photo05, photo06, photo07, photo08, photo09,
-  photo10, photo11, photo12, photo13, photo14, photo15, photo16, photo17, photo18, photo19,
+  photo01, photo02, photo03, photo04, photo05, photo09,
+    photo12, photo15,  photo18, 
 ];
 
 export const couplePhotos = [
-  photo01, photo02, photo03, photo04, photo05, photo06, photo09,
-  photo10, photo13, photo16, photo18, photo19,
+  photo01, photo02, photo03, photo04, photo05, photo09,  photo18, 
 ];
 
 export const heroPhoto = photo05;
